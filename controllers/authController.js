dcb5c8c95b3c560e3c56573a01fde24262b28266
shubhamsarkar9654 <shubhamstark965478@gmail.com>
2020-10-08 20:50:56 +0530

    commit in controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,11 @@ const User = require('../models/users')
 const nodemailer = require('nodemailer')
 const sendgridTransport = require('nodemailer-sendgrid-transport')
 
-const transporter = nodemailer.createTransport(sendgridTransport({
-	auth: {
-		// api_key: 
-	}
-}))
+// const transporter = nodemailer.createTransport(sendgridTransport({
+// 	auth: {
+// 		// api_key: 
+// 	}
+// }))
 
 const getLogin = (req, res) => {
     return res.render('auth/login', {
